bdca3776a157dbe49774f692baec469f8d615ec2
prasad <prasad.parvathapuram@gmail.com>
2017-04-23 16:58:36 +0000

    added basic apachecookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,13 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-
 package 'apache2' do
 	package_name 'httpd'
 	action	:install
 end 
 service 'apache2' do
  	service_name 'httpd'
-	action[:start, :enable]
+	action [:start, :enable]
 end
 	
